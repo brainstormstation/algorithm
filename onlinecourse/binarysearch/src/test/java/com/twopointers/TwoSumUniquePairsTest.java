5ca796c67a4f8c90a224575f662a0557867248c9
Bing <windyboy911@msn.com>
2021-05-12 00:01:25 +0800

    Two Pointers submit

---
+++ onlinecourse/binarysearch/src/test/java/com/twopointers/TwoSumUniquePairsTest.java
@@ -0,0 +1,25 @@
+package com.twopointers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwoSumUniquePairsTest {
+
+    private TwoSumUniquePairs target;
+
+    @Before
+    public void setup() {
+        this.target = new TwoSumUniquePairs();
+    }
+
+    @Test
+    public void testTwoSum6() {
+        int[] arr = new int[] {1,1,2,45,46,46};
+        int sum = 47;
+        int count = this.target.twoSum6(arr, sum);
+        assertEquals(2, count);
+    }
+    
+}
