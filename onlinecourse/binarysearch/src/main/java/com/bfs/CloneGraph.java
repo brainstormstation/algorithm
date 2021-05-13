3040d4f9731137027739b642d728c97c6f94a082
Bing <windyboy911@msn.com>
2021-05-13 23:10:44 +0800

    Add bfs practice.

---
+++ onlinecourse/binarysearch/src/main/java/com/bfs/CloneGraph.java
@@ -0,0 +1,75 @@
+package com.bfs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) {
+        label = x;
+        neighbors = new ArrayList<>();
+    }
+}
+/**
+ * <b>Description</b>
+ * <p>
+ * Clone an undirected graph. 
+ * Each node in the graph contains a label and a list of its neighbors. 
+ * Nodes are labeled uniquely.
+ * You need to return a deep copied graph, 
+ * which has the same structure as the original graph, 
+ * and any changes to the new graph will not have any effect on the original graph.
+ * <p>
+ * https://www.lintcode.com/problem/137/?_from=ladder&fromId=161
+ */
+public class CloneGraph {
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return node;
+        }
+
+        ArrayList<UndirectedGraphNode> nodes = getNodes(node);
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+
+        for (UndirectedGraphNode n: nodes) {
+            map.put(n, new UndirectedGraphNode(n.label));
+        }
+
+        for (UndirectedGraphNode n: nodes) {
+            UndirectedGraphNode newNode = map.get(n);
+            for (UndirectedGraphNode neighbor: n.neighbors) {
+                UndirectedGraphNode newNeighor = map.get(neighbor);
+                newNode.neighbors.add(newNeighor);
+            }
+        }
+        return map.get(node);
+    }
+
+    private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node) {
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        HashSet<UndirectedGraphNode> set = new HashSet<>();
+
+        queue.offer(node);
+        set.add(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode head = queue.poll();
+            for (UndirectedGraphNode neighbor: head.neighbors) {
+                if (!set.contains(neighbor)) {
+                    set.add(neighbor);
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
---
+++ onlinecourse/binarysearch/src/test/java/com/bfs/WordLadderTest.java
@@ -0,0 +1,31 @@
+package com.bfs;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordLadderTest {
+    private WordLadder target;
+
+    @Before
+    public void setup() {
+        this.target = new WordLadder();
+    }
+
+    @Test
+    public void testWordLadder() {
+        String start = "hit";
+        String end = "cog";
+        HashSet<String> dict = new HashSet<String>();
+        dict.add("hot");
+        dict.add("dot");
+        dict.add("dog");
+        dict.add("lot");
+        dict.add("log");
+        int len = this.target.ladderLength(start, end, dict);
+        assertEquals(5, len);
+    }
+}
