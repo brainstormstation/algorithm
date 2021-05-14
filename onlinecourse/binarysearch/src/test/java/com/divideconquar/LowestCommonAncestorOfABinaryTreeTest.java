10df063c3f2274b4a0cf5766ef0f7a096ab4eb7b
Bing <windyboy911@msn.com>
2021-05-14 20:42:05 +0800

    Divide conquar practice.

---
+++ onlinecourse/binarysearch/src/test/java/com/divideconquar/LowestCommonAncestorOfABinaryTreeTest.java
@@ -0,0 +1,20 @@
+package com.divideconquar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LowestCommonAncestorOfABinaryTreeTest {
+    private LowestCommonAncestorOfABinaryTree target;
+
+    @Before
+    public void setup() {
+        this.target = new LowestCommonAncestorOfABinaryTree();
+    }
+
+    @Test
+    public void testLowestCommonAncestorOfABinaryTree() {
+        assertTrue(true);
+    }
+}
