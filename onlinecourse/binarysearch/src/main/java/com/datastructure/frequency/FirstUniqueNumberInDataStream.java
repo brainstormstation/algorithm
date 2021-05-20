44f6be358ccb0e209c11583211b49ac5168d206e
Bing <windyboy911@msn.com>
2021-05-20 20:55:09 +0800

    Frequency practicex

---
+++ onlinecourse/binarysearch/src/main/java/com/datastructure/frequency/FirstUniqueNumberInDataStream.java
@@ -0,0 +1,21 @@
+package com.datastructure.frequency;
+
+/**
+ * <b>Description</b>
+ * <p>
+ * Given a continuous stream of data, write a function that returns the first unique number 
+ * (including the last number) when the terminating number arrives. 
+ * If the terminating number is not found, return -1.
+ * <p>
+ * https://www.lintcode.com/problem/685/?_from=ladder&fromId=161
+ */
+public class FirstUniqueNumberInDataStream {
+    /**
+     * @param nums: a continuous stream of numbers
+     * @param number: a number
+     * @return: returns the first unique number
+     */
+    public int firstUniqueNumber(int[] nums, int number) {
+        // Write your code here
+    }
+}
