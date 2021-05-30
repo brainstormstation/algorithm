d877472b105ed0c5d73d2cf6d5533df41110ed6d
Bing <windyboy911@msn.com>
2021-05-30 14:39:08 +0800

    Two point

---
+++ onlinecourse/binarysearch/src/main/java/com/datastructure/twopointers/MinimumWindowSubstring.java
@@ -0,0 +1,55 @@
+package com.datastructure.twopointers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * <b>Description</b>
+ * <p>
+ * Given two strings s and t of lengths m and n respectively, 
+ * return the minimum window in s which will contain all the characters in t. 
+ * If there is no such window in s that covers all characters in t, return the empty string "".
+ * <p>
+ * <b>Note</b> that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
+ * <p>
+ * https://leetcode.com/problems/minimum-window-substring/
+ */
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        int[] tChars = new int[128];
+        for (char c: t.toCharArray()) {
+            tChars[c]++;
+        }
+        
+        int tlen = t.length();
+        
+        int start = 0;
+        int window = 0;
+        int min = Integer.MAX_VALUE;
+        String result = "";
+        
+        for (int end = 0; end < s.length(); end++) {
+            if (tChars[s.charAt(end)] > 0) {
+                window++;
+            }
+            tChars[s.charAt(end)]--;
+            
+            while (window == tlen) {
+                if (min > end - start + 1) {
+                    min = end - start + 1;
+                    result = s.substring(start, end + 1);
+                }
+                
+                tChars[s.charAt(start)]++;
+                if (tChars[s.charAt(start)] > 0) {
+                    window--;
+                }
+                start++;
+            }
+        }
+        
+        return result;
+    }
+    
+}
---
+++ onlinecourse/binarysearch/src/test/java/com/datastructure/twopointers/MinimumWindowSubstringTest.java
@@ -0,0 +1,45 @@
+package com.datastructure.twopointers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinimumWindowSubstringTest {
+    private MinimumWindowSubstring target;
+
+    @Before
+    public void setup() {
+        this.target = new MinimumWindowSubstring();
+    }
+
+    @Test
+    public void testMinWindow() {
+        String s = "ADOBECODEBANC", t = "ABC";
+        String result = this.target.minWindow(s, t);
+        assertEquals("BANC", result);
+    }
+
+    @Test
+    public void testMinWindowWithDuplicate() {
+        String s = "AA", t = "AA";
+        String result = this.target.minWindow(s, t);
+        System.out.println(result);
+        assertEquals("AA", result);
+    }
+    
+    @Test
+    public void testMinWindow1() {
+        String s = "a", t = "ab";
+        String result = this.target.minWindow(s, t);
+        System.out.println(result);
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testMinWindow2() {
+        String s = "bba", t = "ab";
+        String result = this.target.minWindow(s, t);
+        assertEquals("ba", result);
+    }
+}
