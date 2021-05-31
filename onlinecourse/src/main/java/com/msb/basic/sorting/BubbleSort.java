219b9e3c87eb31ebd5fb6d93bf69609a8c5fc8a5
Bing <windyboy911@msn.com>
2021-05-31 22:59:53 +0800

    Update the structure for the testing.

--- onlinecourse/src/main/java/com/msb/basic/sorting/BubbleSort.java
+++ onlinecourse/src/main/java/com/msb/basic/sorting/BubbleSort.java
@@ -2,15 +2,23 @@ package com.msb.basic.sorting;
 
 public class BubbleSort {
     public void bubbleSort(int[] arr) {
-        if (arr == null || arr.length ==0) {
+        if (arr == null || arr.length < 2) {
             return;
         }
 
         int len = arr.length;
         for (int i=len-1; i>=0; i--) {
-            for (int j=i; j<arr.length; j++) {
-
+            for (int j=1; j<= i; j++) {
+                if (arr[j-1]>arr[j]) {
+                    swap(arr, j-1, j);
+                }
             }
         }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
---
+++ onlinecourse/src/main/java/com/msb/basic/sorting/SelectSort.java
@@ -0,0 +1,26 @@
+package com.msb.basic.sorting;
+
+public class SelectSort {
+    public void selectSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        int len = arr.length;
+
+        for (int i=0; i<len; i++) {
+            int minPos = i;
+            for (int j = i+1; j<len; j++) {
+                minPos = arr[j] < arr[minPos] ? j: minPos;
+            }
+            swap(arr, i, minPos);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
+}
