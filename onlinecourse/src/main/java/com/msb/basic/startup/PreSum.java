9c61dbed9234cbae96d0b312ee15b4de63e22965
Bing <windyboy911@msn.com>
2021-06-03 23:14:43 +0800

    Update the folder name

---
+++ onlinecourse/src/main/java/com/msb/basic/startup/PreSum.java
@@ -0,0 +1,20 @@
+package com.msb.basic.startup;
+
+public class PreSum {
+    private int[] preSum;
+    
+    public PreSum(int[] arr) {
+        int len = arr.length;
+        preSum = new int[len];
+        preSum[0] = arr[0];
+
+        for (int i = 1; i<arr.length; i++) {
+            preSum[i] = preSum[i-1] + arr[i];
+        }
+    }
+
+    public int rangeSum(int start, int end) {
+        return start == 0 ? preSum[start] : (preSum[end] - preSum[start-1]);
+    }
+    
+}
