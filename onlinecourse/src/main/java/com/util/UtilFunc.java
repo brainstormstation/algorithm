762f93c9388a9e778dba3def747315fe32ccee80
Bing <windyboy911@msn.com>
2021-06-30 22:28:05 +0800

    Update the util function.

--- onlinecourse/src/main/java/com/util/UtilFunc.java
+++ onlinecourse/src/main/java/com/util/UtilFunc.java
@@ -6,5 +6,4 @@ public class UtilFunc {
         arr[i] = arr[j];
         arr[j] = tmp;
     }
-    
 }
--- onlinecourse/src/test/java/com/testutil/TestUtil.java
+++ onlinecourse/src/test/java/com/testutil/TestUtil.java
@@ -6,4 +6,12 @@ public class TestUtil {
             System.out.print(" ");
         }
     }
+
+    public static int[] generateRandomArray(int size, int maxValue) {
+        int[] arr = new int[(int)((size+1) * Math.random())];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+        return arr;
+    }
 }
