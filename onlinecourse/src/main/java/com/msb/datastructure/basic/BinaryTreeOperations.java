d8cb8a0c7a288a3bd30ef20fae7255b60c43d68d
Bing <windyboy911@msn.com>
2021-07-19 23:30:21 +0800

    Binary tree operations.

--- onlinecourse/src/main/java/com/msb/datastructure/basic/BinaryTreeOperations.java
+++ onlinecourse/src/main/java/com/msb/datastructure/basic/BinaryTreeOperations.java
@@ -235,4 +235,28 @@ public class BinaryTreeOperations {
         return new FullBSTInfo(nodes, height);
     }
 
+    public boolean isCompleteBinaryTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leaf = false;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if ((node.left == null && node.right != null) || leaf && (node.left!= null || node.right != null)) {
+                return false;
+            }
+            if (node.left == null || node.right == null) {
+                leaf = true;
+            }
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return true;
+    }
 }
