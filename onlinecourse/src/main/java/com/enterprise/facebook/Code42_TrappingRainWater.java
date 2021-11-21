4bbf0077d4796003250d82b51fdeabcdb7894d03
Bing <windyboy911@msn.com>
2021-11-22 01:38:00 +0800

    Add Practice.

---
+++ onlinecourse/src/main/java/com/enterprise/facebook/Code42_TrappingRainWater.java
@@ -0,0 +1,39 @@
+package com.enterprise.facebook;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+ * compute how much water it can trap after raining.
+ */
+public class Code42_TrappingRainWater {
+    public int trap(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = height[0];
+        int rightMax = height[height.length-1];
+        int ans = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                }
+                else {
+                    ans += leftMax - height[left];
+                }
+                left ++;
+            }
+            else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                }
+                else {
+                    ans += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+}
---
+++ onlinecourse/src/main/java/com/enterprise/facebook/Code560_SubarraySumEqualsK.java
@@ -0,0 +1,24 @@
+package com.enterprise.facebook;
+
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer k, 
+ * return the total number of continuous subarrays whose sum equals to k.
+ */
+public class Code560_SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        map.put(0, 1);
+        for (int i=0 ;i< nums.length ;i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count+= map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum - k, 0) + 1);
+        }
+        return count;
+    }
+}
